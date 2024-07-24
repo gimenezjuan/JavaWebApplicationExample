121939f7daea132550e8fa7113af83851bccd13a
gimenezjuan <gimenezcorbalan@gmail.com>
2024-07-24 12:32:30 +0200

    Creado form y botón de bienvenida

--- src/main/java/ejemplos/servlet/curso/MyServlet.java
+++ src/main/java/ejemplos/servlet/curso/MyServlet.java
@@ -11,9 +11,7 @@ import java.io.PrintWriter;
 @WebServlet(urlPatterns = "/myServlet")
 public class MyServlet extends HttpServlet {
 
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 
@@ -23,8 +21,17 @@ public class MyServlet extends HttpServlet {
 		out.println("<body>");
 		out.println("<h1>Ejemplo Servlet</h1>");
 		out.println("<p>Este es un ejemplo en el curso de Java para generar HTML desde un Servlet.</p>");
-		out.println("<p><a href=\"/myServlet2\">Vamos al otro Servlet</a></p>");
+		// FORM
+		out.println("<form action=\"myServlet2\" method=\"GET\">");
+		// NAME
+		out.println("<label for=\"fname\">Nombre:</label><br>");
+		out.println("<input type=\"text\" id=\"fname\" name=\"fname\" value=\"\"><br>");
+		// LAST NAME
+		out.println("<label for=\"lname\">Apellidos:</label><br>");
+		out.println("<input type=\"text\" id=\"lname\" name=\"lname\" value=\"\"><br><br>");
+		// SEND BUTTON
+		out.println("<input type=\"submit\" value=\"Enviar\">");
+		out.println("</form>");
 		out.println("</body></html>");
 	}
-
 }
